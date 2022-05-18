cc9ef997c4ae688d56f7e8ebab2ab99c6ec733f4
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2022-05-19 01:28:40 +0800

    Add PolymeshHooks to contracts.

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -1189,6 +1189,7 @@ impl pallet_contracts::Config for Runtime {
 	type DeletionWeightLimit = DeletionWeightLimit;
 	type Schedule = Schedule;
 	type AddressGenerator = pallet_contracts::DefaultAddressGenerator;
+	type PolymeshHooks = pallet_contracts::DefaultPolymeshHooks;
 	type MaxCodeLen = ConstU32<{ 128 * 1024 }>;
 	type MaxStorageKeyLen = ConstU32<128>;
 	type UnsafeUnstableInterface = ConstBool<false>;
